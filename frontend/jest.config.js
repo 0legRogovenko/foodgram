f801c6754fa237568390666a5398d4766296244c
Oleg Rogovenko <080806oleg@gmail.com>
2026-02-24 22:17:09 +0300

    Modified main.yml

---
+++ frontend/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx}',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-tooltip)/)',
+  ],
+};
